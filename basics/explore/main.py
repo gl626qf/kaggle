9eceaf433cf64fd50ce5d2464809bba310ae13b1
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-03-21 15:01:12 +0100

    first commit

---
+++ basics/explore/main.py
@@ -0,0 +1,20 @@
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex2 import *
+print("Setup Complete")
+
+#Importing library
+import dataloader.py
+
+
+'''
+Now I want to call the dataloader 
+'''
+
+
+step_1.check()
+
+
+
+
